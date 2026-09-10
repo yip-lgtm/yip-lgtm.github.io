452fb270d4c21c8f5a1d2445a3cfdfa2badae364
yip-lgtm <yip@ktsurveyors.com>
2026-09-11 01:43:59 +0800

    Weight page: lock 4 free models in ranked order

--- yipmma/weight/block-minimax.js
+++ yipmma/weight/block-minimax.js
@@ -1,9 +1,9 @@
 (function () {
   var FREE = [
-    "nvidia/nemotron-3.5-lightning:free",
-    "thinkingmachines/inkling-small:free",
+    "nvidia/nemotron-3-super-120b-a12b:free",
+    "google/gemma-4-31b-it:free",
     "inclusionai/ling-3.0-flash-sante:free",
-    "google/gemma-4-31b-it:free"
+    "nvidia/nemotron-3.5-lightning:free"
   ];
   var orig = window.fetch;
   window.fetch = function (url, opt) {
@@ -13,7 +13,7 @@
       try {
         var b = JSON.parse(opt.body);
         var m = String(b.model || "");
-        if (!m || m.indexOf("minimax") !== -1 || m === "openrouter/free" || m.indexOf(":free") === -1) {
+        if (!m || m.indexOf("minimax") !== -1 || m === "openrouter/free" || FREE.indexOf(m) === -1) {
           b.model = FREE[0];
           opt = Object.assign({}, opt, { body: JSON.stringify(b) });
         }
@@ -26,8 +26,8 @@
           var b2;
           try { b2 = JSON.parse(opt.body || "{}"); } catch (e) { b2 = {}; }
           var cur = b2.model || "";
-          var next = FREE.filter(function (x) { return x !== cur; })[0] || FREE[0];
-          b2.model = next;
+          var idx = FREE.indexOf(cur);
+          b2.model = FREE[(idx + 1) % FREE.length];
           return orig.call(window, url, Object.assign({}, opt, { body: JSON.stringify(b2) }));
         }
         return res;
